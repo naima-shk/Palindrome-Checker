859649f4a2c89047d090408aefb863eb8dfa72e5
shyam tayal <shyam2tayal@gmail.com>
2021-06-30 14:23:24 +0530

    Better solution

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 function isPalin(){
-    let x= document.getElementById('a').value;
-    let y= '';
-    for (i=x.length-1; i>=0; i--){
-        y=y+x[i];
-    }
-    if(x==y)
-        document.getElementById('b').value=x+ 'is a palindrome';
-     else
-        document.getElementById('b').value=x+ 'is  not a palindrome';
-    
+    let str = document.getElementById('a').value;
+    document.getElementById('b').value=str + ` is ${str != str.split('').reverse().join('') ? 'not ' : ''}a palindrome`;
 }
